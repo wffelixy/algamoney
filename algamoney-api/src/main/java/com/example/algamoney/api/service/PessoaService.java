3c11d59e095e1dd4cc6e96f57a8aa3f43eda14fa
Wellington Felix <wffelixy@gmail.com>
2023-05-23 18:19:25 -0300

    Implemento atualização parcial de pessoa

--- algamoney-api/src/main/java/com/example/algamoney/api/service/PessoaService.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/service/PessoaService.java
@@ -17,13 +17,26 @@ public class PessoaService {
 	private PessoaRepository pessoaRepository;
 	
 	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
+	
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		pessoaRepository.save(pessoaSalva);
+	}
+
+	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
 		Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);
 		if (pessoaSalva == null || pessoaSalva.isEmpty()) {
 			throw new EmptyResultDataAccessException(1);
 		}
-		
-		BeanUtils.copyProperties(pessoa, pessoaSalva.get(), "codigo");
-		return pessoaRepository.save(pessoaSalva.get());
+		return pessoaSalva.get();
 	}
+
+	
 	
 }
