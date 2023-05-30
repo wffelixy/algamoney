fe4b6aca66164a65de5ec3f3581af4ddb2a81c84
Wellington Felix <wffelixy@gmail.com>
2023-05-30 14:09:42 -0300

    Implementado autenticação basic

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.algamoney.api.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("admin")
+            .roles("ROLE")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> {
+				try {
+					authz
+					    .requestMatchers("/categorias").permitAll()
+					    .anyRequest().authenticated().and()
+					    .httpBasic().and()
+					    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+					    .csrf().disable();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+            )
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+}
